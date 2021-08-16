a7063d4f2dbe86e5c9fcd58a3cd31bce92b51657
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-16 13:26:00 +0300

    handled registrtion req and added password hashing

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,31 @@
 const { Router } = require('express')
 const router = Router()
 
-//auth/register
-router.post('/register', (req, res) => {
+const User = require('../models/User')
 
+//auth/register
+router.post('/register', async (req, res) => {
+   try {
+      const { email, password } = req.body
+
+      const registeredUser = await User.findOne({ email })
+      if (!registeredUser) {
+         return res.status(400).json({ message: 'Такой пользователь уже есть в БД' })
+      }
+
+      const hashedPassword = await bcript.hash(password, 10, e => {
+         if (err) {
+            return res.status(500).json({ message: e.message })
+         }
+      })
+
+      const user = new User({ email, password: hashedPassword })
+      await user.save()
+
+      res.status(201).json({ message: 'Пользователь был создан' })
+   } catch (e) {
+      res.status(500).json({ message: e.message })
+   }
 })
 
 router.post('/login', (req, res) => {
