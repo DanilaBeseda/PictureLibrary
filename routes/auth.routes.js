38939b65b22ccdc814f1753389f2a0bfa161233d
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-16 14:06:35 +0300

    handle login req and added jwt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const jwt = require('jsonwebtoken')
 const router = Router()
 
 const User = require('../models/User')
@@ -9,15 +10,11 @@ router.post('/register', async (req, res) => {
       const { email, password } = req.body
 
       const registeredUser = await User.findOne({ email })
-      if (!registeredUser) {
+      if (registeredUser) {
          return res.status(400).json({ message: 'Такой пользователь уже есть в БД' })
       }
 
-      const hashedPassword = await bcript.hash(password, 10, e => {
-         if (err) {
-            return res.status(500).json({ message: e.message })
-         }
-      })
+      const hashedPassword = await bcript.hash(password, 10)
 
       const user = new User({ email, password: hashedPassword })
       await user.save()
@@ -28,8 +25,28 @@ router.post('/register', async (req, res) => {
    }
 })
 
+//auth/login
 router.post('/login', (req, res) => {
+   try {
+      const { email, password } = req.body
 
+      const registeredUser = await User.findOne({ email })
+      if (!registeredUser) {
+         return res.status(400).json({ message: 'Пользователь не найден' })
+      }
+
+      const isMatch = bcrypt.compare(password, registeredUser.password)
+      if (!isMatch) {
+         return res.status(400).json({ message: 'Некорректные данные' })
+      }
+
+      const token = jwt.sign({ userId: registeredUser._id }, config.get('jwtSecret'), { expiresIn: '1h' })
+
+      res.json({ token, userId: registeredUser._id })
+
+   } catch (e) {
+      res.status(500).json({ message: e.message })
+   }
 })
 
 module.exports = router
