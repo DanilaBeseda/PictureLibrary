05495204a811264b8608ed419d1cad7c3dab5311
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-19 12:33:01 +0300

    added auth.hook for automatic login

---
+++ client/src/hooks/auth.hook.js
@@ -0,0 +1,30 @@
+import { useState, useCallback, useEffect } from 'react'
+
+export const useAuth = () => {
+   const [token, setToken] = useState(null)
+   const [userId, setUserId] = useState(null)
+
+   const signIn = useCallback((jwtToken, id) => {
+      setToken(jwtToken)
+      setUserId(id)
+
+      localStorage.setItem('userData', JSON.stringify({ userId: id, token: jwtToken }))
+   }, [])
+
+   const signOut = useCallback(() => {
+      setToken(null)
+      setUserId(null)
+
+      localStorage.removeItem('userData')
+   })
+
+   useEffect(() => {
+      const data = JSON.parse(localStorage.getItem('userData'))
+
+      if (data && data.token) {
+         signIn(data.token, data.userId)
+      }
+   }, [signIn])
+
+   return { signIn, signOut, token, userId }
+}
