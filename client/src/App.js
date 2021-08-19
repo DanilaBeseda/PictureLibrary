9e6129fb5b46231f247e294f9a2ae1847b54e838
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-19 12:44:21 +0300

    added AuthContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import { ToastContainer } from 'react-toastify'
 
 import { AuthPage } from './pages/AuthPage'
+import { AuthContext } from './context/AuthContext'
+import { useAuth } from './hooks/auth.hook';
 
 import './styles/App.scss'
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const { signIn, signOut, token, userId } = useAuth()
+
   return (
     <>
       <ToastContainer />
-      <div className='pages'>
-        <AuthPage />
-      </div>
+      <AuthContext.Provider value={signIn, signOut, token, userId}>
+        <div className='pages'>
+          <AuthPage />
+        </div>
+      </AuthContext.Provider>
     </>
   )
 }
