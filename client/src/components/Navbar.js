c4dd457eb9c63051c95340377bb8c7083e7ebc45
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-19 19:27:28 +0300

    dixed navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,12 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 import '../styles/Navbar.scss'
 
 export const Navbar = () => {
+   const NavLinkStyles = {
+      color: '#fff',
+      boxShadow: '#fff 0px 0px 7px 0px'
+   }
 
    return (
       <div className='navbar'>
@@ -12,9 +16,9 @@ export const Navbar = () => {
                   <span>Picture Library</span>
                </div>
                <div className='navbar__links'>
-                  <NavLink to='/library' activeStyle={{ color: '#fff' }}>library</NavLink>
-                  <NavLink to='/addpicture' activeStyle={{ color: '#fff' }}>add picture</NavLink>
-                  <NavLink to='/signout' activeStyle={{ color: '#fff' }}>sign out</NavLink>
+                  <NavLink to='/library' activeStyle={NavLinkStyles}>library</NavLink>
+                  <NavLink to='/addpicture' activeStyle={NavLinkStyles}>add picture</NavLink>
+                  <NavLink to='/signout' activeStyle={NavLinkStyles}>sign out</NavLink>
                </div>
             </div>
          </div>
