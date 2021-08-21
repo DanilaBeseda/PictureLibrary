de663ac8c7f14e733549b8b5332c5cb601488288
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-21 14:13:58 +0300

    add new router

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const app = express()
 app.use(express.json())
 
 app.use('/auth', require('./routes/auth.routes'))
+app.use('/addpicture', require('./routes/addPicture.routes'))
 
 const PORT = config.get('port') || 5000
 
 async function start() {
    try {
-      await mongoose.connect(config.get('mongoURI'), { useNewUrlParser: true, useUnifiedTopology: true })
+      await mongoose.connect(config.get('mongoURI'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
       app.listen(PORT, () => {
          console.log(`Server running on port ${PORT}...`)
