ed45177aa591ea78cb9fc1f82c13265153db9539
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-21 14:13:45 +0300

    created Schema for Pictures

---
+++ models/Picture.js
@@ -0,0 +1,9 @@
+const { Schema, model, Types } = require('mongoose')
+
+const schema = new Schema({
+   name: { type: String, required: true, unique: true },
+   url: { type: String, required: true },
+   userId: { type: Types.ObjectId, ref: 'User' }
+})
+
+module.exports = model('Picture', schema)
