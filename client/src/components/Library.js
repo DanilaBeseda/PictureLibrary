a6a88d02950c5bba5731d6ba63ada7e7b1dd7c26
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-21 18:04:54 +0300

    create library page

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,9 +1,43 @@
+import { useCallback, useContext, useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
+
 import '../styles/Library.scss'
 
 export const Library = () => {
+   const { request } = useHttp()
+   const { signOut, token } = useContext(AuthContext)
+   const [pictures, setPictures] = useState(null)
+
+   const getData = useCallback(async () => {
+      try {
+         const data = await request('/library', 'GET', null, {
+            Authorization: `Bearez ${token}`
+         })
+         setPictures(data)
+      } catch (e) {
+         toast.error(e.message)
+         if (e.message === 'jwt expired') {
+            signOut()
+         }
+      }
+   }, [request, signOut, token])
+
+   useEffect(() => {
+      getData()
+   }, [getData])
+
    return (
-      <div className='library'>
-         <h1>Library</h1>
+      <div className='container'>
+         <div className='library'>
+            {pictures && pictures.map((picture, index) => (
+               <div key={index} className='library__item'>
+                  <img src={picture.url} alt={picture.name} />
+               </div>
+            ))}
+         </div>
       </div>
    )
 }
