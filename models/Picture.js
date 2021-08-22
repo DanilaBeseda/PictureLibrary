c2918919287495e8adde9a3708f5411c376888c5
Danila Beseda <danila.beseda1999@gmail.com>
2021-08-22 15:45:13 +0300

    returned unique

--- models/Picture.js
+++ models/Picture.js
@@ -1,7 +1,7 @@
 const { Schema, model, Types } = require('mongoose')
 
 const schema = new Schema({
-   name: { type: String, required: true },
+   name: { type: String, required: true, unique: true },
    url: { type: String, required: true },
    userId: { type: Types.ObjectId, ref: 'User' }
 })
